c2155b39bf029865cea8dc9b263cfe1befa0cfbc
mahyar <mahyar_066@yahoo.com>
2024-11-14 14:07:53 +0330

    Second commit

--- exercise/blue_sky.py
+++ exercise/blue_sky.py
@@ -3,15 +3,20 @@ import pygame
 from settings import Settings
 from rocket import Rocket
 
+
 class BlueSky:
     def __init__(self):
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width,
-             self.settings.screen_height)
-        )
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+
+        # self.screen = pygame.display.set_mode(
+        #     (self.settings.screen_width,
+        #      self.settings.screen_height)
+        # )
 
         self.rocket = Rocket(self)
         self.clock = pygame.time.Clock()
@@ -20,13 +25,52 @@ class BlueSky:
     def run_game(self):
         while True:
             self._check_events()
+            self.rocket.update()
             self._update_screen()
-            self.clock.tick(60)
+            self.clock.tick(120)
+
 
     def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.rocket.moving_right = True
+
+        if event.key == pygame.K_LEFT:
+            self.rocket.moving_left = True
+
+        if event.key == pygame.K_UP:
+            self.rocket.moving_up = True
+
+        if event.key == pygame.K_DOWN:
+            self.rocket.moving_down = True
+
+        if event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.rocket.moving_right = False
+
+        if event.key == pygame.K_LEFT:
+            self.rocket.moving_left = False
+
+        if event.key == pygame.K_UP:
+            self.rocket.moving_up = False
+
+        if event.key == pygame.K_DOWN:
+            self.rocket.moving_down = False
+
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
--- exercise/rocket.py
+++ exercise/rocket.py
@@ -6,10 +6,33 @@ class Rocket:
         self.screen = bs_game.screen
         self.screen_rect = bs_game.screen.get_rect()
 
-        self.image = pygame.image.load('ship_edit.png')
+        self.image = pygame.image.load('ship_edit.png').convert_alpha()
         self.rect = self.image.get_rect()
 
         self.rect.center = self.screen_rect.center
+
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.x += 1
+            print(self.rect.x, self.screen_rect.right)
+
+        if self.moving_left and self.rect.left > 0:
+            self.rect.x -= 1
+            print(self.rect.x, self.screen_rect.left)
+
+        if self.moving_up and self.rect.top > self.screen_rect.top:
+            self.rect.y -= 1
+            print(self.rect.y, self.screen_rect.top)
+
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.rect.y += 1
+            print(self.rect.y, self.screen_rect.bottom)
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
